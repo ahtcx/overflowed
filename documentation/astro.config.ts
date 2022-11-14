c74beb3f9b29efc8d460a704534da27ceb260298
Alexandre Hitchcox <alexandre.hitchcox@livenation.co.uk>
2022-11-14 18:33:53 +0000

    holy shit it works

--- documentation/astro.config.ts
+++ documentation/astro.config.ts
@@ -4,15 +4,17 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-const a = import.meta.glob([
-	"../node_modules/.pnpm/**/shiki/languages/*.tmLanguage.json",
-	"../node_modules/.pnpm/**/shiki/themes/*.json",
-]);
+const shikiResourcePaths = Object.keys(
+	import.meta.glob([
+		"../node_modules/.pnpm/**/shiki/languages/*.tmLanguage.json",
+		"../node_modules/.pnpm/**/shiki/themes/*.json",
+	]),
+);
 
 export default defineConfig({
 	integrations: [mdx(), react()],
 	output: "server",
 	adapter: vercel({
-		includeFiles: Object.keys(a),
+		includeFiles: shikiResourcePaths,
 	}),
 });
