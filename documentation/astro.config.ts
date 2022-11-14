669c8bd4031baadd8f74bb5b929bc6aaaf532112
Alexandre Hitchcox <alexandre.hitchcox@livenation.co.uk>
2022-11-14 18:40:10 +0000

    k this is the one then

--- documentation/astro.config.ts
+++ documentation/astro.config.ts
@@ -4,12 +4,11 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
+// FIXME: https://github.com/withastro/astro/issues/5357
 const shikiResourcePaths = Object.keys(
 	import.meta.glob([
-		// "../node_modules/.pnpm/shiki@*/node_modules/shiki/languages/*.tmLanguage.json",
-		// "../node_modules/.pnpm/shiki@*/node_modules/shiki/themes/*.json",
-		"./node_modules/shiki/languages/*.tmLanguage.json",
-		"./node_modules/shiki/themes/*.json",
+		"../node_modules/.pnpm/shiki@*/node_modules/shiki/languages/*.tmLanguage.json",
+		"../node_modules/.pnpm/shiki@*/node_modules/shiki/themes/*.json",
 	]),
 );
 
