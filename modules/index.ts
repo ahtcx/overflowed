3666ec294dafa94d419cd4a2d9a9ba12ba9c8b2e
Alexandre Hitchcox <alexandre.hitchcox@livenation.co.uk>
2022-11-20 12:57:29 +0000

    add license and error

--- modules/index.ts
+++ modules/index.ts
@@ -1,2 +1,6 @@
 /** @deprecated Don't export this. */
 export default undefined;
+
+throw new Error(
+	"You imported `overflowed` directly, instead you should import from it's subdirectories such as `overflowed/react` or `overflowed/core`. Documentation for this error is available at https://overflowed.aht.cx/errors/imported-root",
+);
