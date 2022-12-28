2d648f580399c4f8d29cf87c2e7518f2b0e579a3
Alexandre Hitchcox <alexandre@hitchcox.me>
2022-12-28 18:46:50 +0000

    yo

--- documentation/astro.config.ts
+++ documentation/astro.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
 	integrations: [mdx(), react(), svelte()],
-	output: "server",
+	output: "static",
 	adapter: vercel(),
 });
